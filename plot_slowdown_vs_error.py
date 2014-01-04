3d7cbf70ec51eb3d9a83f013371bfb45e2fad821
Matteo Dell'Amico <matteodellamico@gmail.com>
2014-01-04 13:55:12 +0100

    modified plot to show plots for arbitrary workloads

--- plot_slowdown_vs_error.py
+++ plot_slowdown_vs_error.py
@@ -18,15 +18,23 @@ NPOINTS = 100000
 
 parser = argparse.ArgumentParser(description="produce CDFs for slowdown "
                                  "vs. error")
-parser.add_argument('dataset', help="name of the .tsv file used.")
+parser.add_argument('dataset', help="name of the schedule file used or "
+                    "TSV dataset.")
+parser.add_argument('--tsv', default=False, action='store_true',
+                    help="Read results produced from a SWIM .tsv file")
+parser.add_argument('--fname',
+                    help="fetch the results from this file. "
+                    "Needed unless --tsv is set")
 parser.add_argument('--sigma', type=float, default=0.5, help="sigma parameter "
-                    "for the log-normal error function; default is 0.5")
+                    "for the log-normal error function; default is 0.5. "
+                    "Ignored unless --tsv is set")
 parser.add_argument('-dn', '--d-over-n', dest="d_over_n", type=float,
                     default=4.0, help="ratio between disk and network "
-                    "bandwidth in the simulated cluster; default is 4")
+                    "bandwidth in the simulated cluster; default is 4. "
+                    "Ignored unless --tsv is set")
 parser.add_argument('--load', type=float, default=0.9,
                     help="average load in the simulated cluster; default is "
-                    "0.9")
+                    "0.9. Ignored unless --tsv is set")
 parser.add_argument('--paper', dest='for_paper', action='store_const',
                     const=True, default=False, help="render plots with "
                     "LaTeX and output them as "
@@ -36,15 +44,24 @@ args = parser.parse_args()
 if args.for_paper:
     plot_helpers.config_paper()
 
-fname_fmt = 'results_{}_{}_{}_{}.s'
-fname = fname_fmt.format(args.dataset, args.sigma, args.d_over_n, args.load)
+if not args.fname:
+    fname_fmt = 'results_{}_{}_{}_{}.s'
+    fname = fname_fmt.format(args.dataset, args.sigma, args.d_over_n, args.load)
+else:
+    fname = args.fname
 
 no_error = ['FIFO', 'PS', 'LAS', 'FSP (no error)', 'SRPT (no error)']
 with_error = ['LAS', 'FSP + FIFO', 'FSP + PS', 'SRPT',
               'SRPT + PS', 'FSP + LAS', 'SRPT + LAS']
 
-workload = swim_parser.parse_swim('{}.tsv'.format(args.dataset),
-                                  args.d_over_n, args.load)
+if args.tsv:
+    workload_fname = '{}.tsv'.format(args.dataset)
+    workload = swim_parser.parse_swim(workload_fname, args.d_over_n, args.load)
+else:
+    with open(args.dataset) as f:
+        workload = (line.strip().split() for line in f)
+        workload = [(jobid, float(t), float(size))
+                    for jobid, t, size in workload]
 
 job_multipliers = np.array([1 / size if size else 0 for _, _, size in workload])
 
