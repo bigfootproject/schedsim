c93f6547af8892aeba5ebd03c1e145694896afd5
Matteo Dell'Amico <della@linux.it>
2014-10-07 15:37:41 +0200

    allow performing experiments with fixed error data

--- experiment.py
+++ experiment.py
@@ -17,12 +17,16 @@ parser = argparse.ArgumentParser(description="run the experiment; "
                                  "details on parameters in our TR "
                                  "at http://arxiv.org/abs/1306.6023")
 parser.add_argument('file', help="file with the trace to use")
-parser.add_argument('sigma', type=float,
-                    help="sigma parameter for the log-normal error function")
 parser.add_argument('iterations', type=int,
                     help="Iterations for each run of the experiment. "
                     "If that number of iterations is already in the "
                     "results file, nothing will be done.")
+parser.add_argument('--read_estimations', default=False, action='store_true',
+                    help="Read job size estimations from the input file. "
+                    "Ignored if --parse_swim is set")
+parser.add_argument('--sigma', type=float, default=0.5,
+                    help="sigma parameter for the log-normal error function; "
+                    "default: 0.5. Ignored if --read_estimations is set.")
 parser.add_argument('--parse_swim', dest='parse_swim', default=False,
                     action='store_true',
                     help="parse data from a SWIM .tsv file")
@@ -43,11 +47,20 @@ if args.parse_swim:
 else:
     with open(args.file) as f:
         jobs = (line.strip().split() for line in f)
+        if args.read_estimations:
+            args.sigma = None
+            old_jobs = jobs
+            jobs, estimations = [], []
+            for job in old_jobs:
+                jobs.append(job[:-1])
+                estimations.append(float(job[-1]))
         if args.nojobid:
             jobs = ((i, t, size) for i, (t, size) in enumerate(jobs))
         jobs = [(jobid, float(t), float(size)) for jobid, t, size in jobs]
 
-error = simulator.lognorm_error(args.sigma)
+error = (lambda: simulator.fixed_estimations(estimations)
+         if args.read_estimations
+         else lambda: simulator.lognorm_error(args.sigma))
 
 instances = [
     ('FIFO', schedulers.FIFO, simulator.identity, None),
@@ -55,15 +68,15 @@ instances = [
     ('SRPT (no error)', schedulers.SRPT, simulator.identity, None),
     ('FSP (no error)', schedulers.FSP, simulator.identity, None),
     ('LAS', schedulers.LAS, simulator.identity, None),
-    ('SRPT', schedulers.SRPT, error, args.iterations),
-#    ('SRPT + PS', schedulers.SRPT_plus_PS, error, args.iterations),
-    ('FSP + FIFO', schedulers.FSP, error, args.iterations),
-    ('FSP + PS', schedulers.FSP_plus_PS, error, args.iterations),
-#    ('FSP + LAS', schedulers.FSP_plus_LAS, error, args.iterations),
-#    ('SRPT + LAS', schedulers.SRPT_plus_LAS, error, args.iterations),
+    ('SRPT', schedulers.SRPT, error(), args.iterations),
+    ('SRPT + PS', schedulers.SRPT_plus_PS, error(), args.iterations),
+    ('FSP + FIFO', schedulers.FSP, error(), args.iterations),
+    ('FSP + PS', schedulers.FSP_plus_PS, error(), args.iterations),
+    ('FSP + LAS', schedulers.FSP_plus_LAS, error(), args.iterations),
+    ('SRPT + LAS', schedulers.SRPT_plus_LAS, error(), args.iterations),
     ]
 
-jobids = [jobid for jobid, _, _ in jobs]
+jobids = [j[0] for j in jobs]
 job_idxs = {jobid: i for i, jobid in enumerate(jobids)}
 n_jobs = len(jobids)
 
--- renorm_trace.py
+++ renorm_trace.py
@@ -12,12 +12,22 @@ parser = argparse.ArgumentParser(description="Renormalize trace by scaling "
 parser.add_argument('load', type=float, help="desired load")
 parser.add_argument('--input', help="input file (stdin if omitted)")
 parser.add_argument('--output', help="output file (stdout if omitted)")
+parser.add_argument('--renorm_estimations', choices=['proportional', 'total'],
+                    help="renormalize the estimations as well: if "
+                    "'proportional', they will be rescaled by the same factor "
+                    "of sizes; if 'total', the sum of estimations will "
+                    "be equal to the sum of job sizes")
 args = parser.parse_args()
 
 schedule = np.loadtxt(args.input if args.input is not None else sys.stdin)
 
 schedule[:, 0] -= schedule[0, 0]
-schedule[:, 1] *= schedule[-1, 0] * args.load / schedule[:, 1].sum()
+factor = schedule[-1, 0] * args.load / schedule[:, 1].sum()
+schedule[:, 1] *= factor
+if args.renorm_estimations == 'proportional':
+    schedule[:, 2] *= factor
+elif args.renorm_estimations == 'total':
+    schedule[:, 2] *= schedule[:, 1].sum() / schedule[:, 2].sum()
 
 np.savetxt(args.output if args.output is not None else sys.stdout,
            schedule)
